14969d83536f07164fb14010cec7bd875be0944e
Angelo Amati <amati.angelo@20m2official.it>
2023-09-29 13:05:29 +0200

    add reservation form component as standalone component

---
+++ src/app/modules/admin/reservationform/reservationform.component.ts
@@ -0,0 +1,12 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+    selector: 'app-reservationform',
+    standalone   : true,
+    templateUrl: './reservationform.component.html',
+    encapsulation: ViewEncapsulation.None,
+})
+export class ReservationformComponent {
+    constructor() {
+    }
+}
---
+++ src/app/modules/admin/reservationform/reservationforma.routers.ts
@@ -0,0 +1,9 @@
+import {Routes} from "@angular/router";
+import {ReservationformComponent} from "./reservationform.component";
+
+export default [
+    {
+        path     : '',
+        component: ReservationformComponent,
+    },
+] as Routes;
